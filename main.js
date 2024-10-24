5697b8a49cc74966edaf1d4ea753dc55f6082caf
Nomekuma <t.guness35@gmail.com>
2024-10-24 10:51:21 +0400

    add responsive hamburger menu and update navbar styles in index.html, main.js, and style.css

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
+const hamburgerIcon = document.querySelector(".hamburger i");
 
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("active");
+
+  // Toggle between hamburger and cross icon
+  if (menu.classList.contains("active")) {
+    hamburgerIcon.classList.remove("fa-bars");
+    hamburgerIcon.classList.add("fa-times");
+  } else {
+    hamburgerIcon.classList.remove("fa-times");
+    hamburgerIcon.classList.add("fa-bars");
+  }
 });
-
-
